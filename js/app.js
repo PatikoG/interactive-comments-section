bb86b9cb693d80a3e358fc154e16aec4573ba547
patiko <gharibashvilip@gmail.com>
2022-08-31 23:15:40 +0400

    P

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import myJson from "../data.json" assert { type: "json" };
 
-const wrapper = document.querySelector(".wrapper");
+const wrapper = document.querySelector(".forcenter");
 
 for (let i = 0; i < myJson.comments.length; i++) {
   let comment = myJson.comments[i];
@@ -29,8 +29,13 @@ for (let i = 0; i < myJson.comments.length; i++) {
     </div>   
     
 
-    
+      
+        
         <div class="commentsection"></div>
+
+      
+
+
     `;
 
     const comsection = document.querySelectorAll(".commentsection");
@@ -41,23 +46,32 @@ for (let i = 0; i < myJson.comments.length; i++) {
         comsection[i].innerHTML += `
         <div class="ramses">
             <div class="headers">
-                <img src="${reply.user.image.png}">
+                <img src="${reply.user.image.png}" class="imgramsjul">
                 <span class="ramsesmiron">${reply.user.username}</span>
+                ${reply.user.username ===myJson.currentUser.username ? "<span class='you'>you</span> ":"" } 
                 <p class="date">${reply.createdAt}
             </div>
 
             <div class="textsreplies">
-                <p class="text">${reply.content} </p>
+                <p class="text"><span class="mention">@${reply.user.username}</span> ${reply.content} </p>
             </div>
             <div class="endfoot">
                 <div class="plusminus">
-                    <img class="imgplus" src="./images/icon-plus.svg">
+                    <img class="imgplus" ; src="./images/icon-plus.svg">
                     <span class="score">${reply.score} </span>
-                    <img class="imgminus" src="./images/icon-minus.svg">
+                    <img class="imgminus"  src="./images/icon-minus.svg">
                 </div>
                 <div class="replybtn">
-                    <img src="./images/icon-reply.svg">
-                    <p class="reply">reply</p>
+                    
+                    ${reply.user.username === myJson.currentUser.username ? `
+                      <img src='./images/icon-delete.svg'>
+                      
+                      <span class='delete'> delete</span>
+                      <img src='./images/icon-edit.svg'>
+                      <span class='edit'> edit</span>
+                      
+                      `:`<img src="./images/icon-reply.svg">
+                       <p class="reply">reply</p>`} 
                 </div>
 
           </div>
@@ -66,17 +80,61 @@ for (let i = 0; i < myJson.comments.length; i++) {
         
         
         
+ 
+
         `
-        
-
 
 
 
 }
 
+
 }
 
 
+const incrementCount = document.querySelectorAll(".imgplus");
+const decrementCount = document.querySelectorAll(".imgminus");
+const totalCount = document.querySelectorAll(".score");
+
+
+
+
+
+
+
+
+
+
+const handleIncrement = (index) => { 
+  
+  
+  const grade = totalCount[index]
+  grade.textContent++;
+};
+
+
+
+const handleDecrement = (index) => {
+  
+  
+  const grade = totalCount[index]
+  grade.textContent--;
+};
+
+for(let i = 0; i < incrementCount.length; i++){
+  incrementCount[i].addEventListener("click",function(){
+    handleIncrement(i);
+  });
+}
+
+
+
+
+for(let i = 0; i < decrementCount.length; i++){
+  decrementCount[i].addEventListener("click", function(){
+    handleDecrement(i);
+  });
+}
 
 
 
